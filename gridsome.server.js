4e3f54722a743f665fe4654a3071ac4b8bffc70b
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2021-05-29 08:26:45 +0200

    Update server script

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,33 +1,32 @@
-const cw = require("capture-website");
-const crypto = require("crypto");
+const cw = require('capture-website')
+const crypto = require('crypto')
 
-const projects = require("./projects.json");
+const projects = require('./projects.json')
 
 module.exports = function(api) {
-  api.loadSource(async actions => {
-    const contentTypeProjects = actions.addCollection("Project");
+  api.loadSource(async (actions) => {
+    const contentTypeProjects = actions.addCollection('Project')
 
     for (const [index, project] of projects.entries()) {
-      const randomId = crypto.randomBytes(12).toString("hex");
+      const randomId = crypto.randomBytes(12).toString('hex')
       try {
         await cw.file(project.url, `./src/assets/img/${randomId}.png`, {
           scaleFactor: 1,
-          timeout: 360
-        });
-        project.image = `${randomId}.png`;
+          timeout: 360,
+        })
+        project.image = `${randomId}.png`
+        contentTypeProjects.addNode({
+          id: index,
+          name: project.name,
+          image: project.image,
+          description: project.description,
+          url: project.url,
+          source: project.source,
+          tags: project.tags,
+        })
       } catch (err) {
-        console.log(`Capture website error: project ${project.name}`, err);
+        console.error(`Capture website error: project ${project.name}`, err)
       }
-
-      contentTypeProjects.addNode({
-        id: index,
-        name: project.name,
-        image: project.image,
-        description: project.description,
-        url: project.url,
-        source: project.source,
-        tags: project.tags
-      });
     }
-  });
-};
+  })
+}
